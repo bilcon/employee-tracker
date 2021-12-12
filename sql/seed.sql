d31050319ccb9af42e803d0396e0e86d6ba14601
Bill Morris <bilcon@swbell.net>
2021-12-11 19:08:19 -0600

    seeded department and role

---
+++ sql/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (name)
+VALUES ("Sales", "Engineering", "Finance", "Legal");
+
+INSERT INTO role
+ (title, salary, department_id)
+ VALUES
+  ('Sales Lead', 100000, 1),
+  ('Lead Engineer', 150000, 2),
+  ('Software Engineer', 120000, 2),
+  ('Accountant', 125000, 3),
+  ('Legal Team Lead', 250000, 4);
+  
