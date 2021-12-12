a732a72fe6383b570f61e4879eba6fabaa545e1e
Bill Morris <bilcon@swbell.net>
2021-12-12 17:10:28 -0600

    updated server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const express = require("express");
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
