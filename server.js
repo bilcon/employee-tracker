b0f0dbbc8ffefe476fad9aa16912ff15d983c723
Bill Morris <bilcon@swbell.net>
2021-12-12 18:53:39 -0600

    added role questions

--- server.js
+++ server.js
@@ -71,3 +71,60 @@ function startPrompt(){
 if(err)throw err;
 });
 }
+
+function addRole(){
+  var query = 
+  `SELECT 
+    department.id, 
+    department.name, 
+    role.salary
+  FROM employee
+  JOIN role
+    ON employee.role_id = role.id
+  JOIN department
+    ON department.id = role.department_id
+  GROUP BY department.id, department.name`
+
+  connection.query(query,(err, res)=>{
+    if(err)throw err;
+    const department = res.map(({ id, name }) => ({
+      value: id,
+      name: `${id} ${name}`
+    }));
+    console.table(res);
+    addToRole(department);
+  });
+}
+
+function addToRole(department){
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Role title: "
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Role Salary: "
+      },
+      {
+        type: "list",
+        name: "department",
+        message: "Department: ",
+        choices: department
+      },
+    ]).then((res)=>{
+      let query = `INSERT INTO role SET ?`;
+
+      connection.query(query, {
+          title: res.title,
+          salary: res.salary,
+          department_id: res.department
+      },(err, res)=>{
+          if(err) throw err;
+          firstPrompt();
+      });
+  });
+}
